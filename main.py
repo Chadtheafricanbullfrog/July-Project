00d3817876954b7d905beeb92ab30ed19dc7b254
BuildTools <unconfigured@null.spigotmc.org>
2019-07-01 16:01:32 -0700

    Began python questions

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import pygame
+from pygame.locals import*
+
+pygame.init()
+screen = pygame.display.info()
+
+#set width and height to screen size
+size = (width,height) = (int(screen.current_w), int(screen.current_h)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+color = (0, 127, 225)
+
 def main():
-    print("hello world")
-
-
+    while True:
+        clock.tick(60)
+        screen.fill(color)
+        pygame.display.flip()
 
 
 
